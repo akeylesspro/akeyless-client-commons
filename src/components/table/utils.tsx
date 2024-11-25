f72301e0c61f99b3f50b346fb279f1a42570b19e
avrhahm sason <avi6190i@gmail.com>
2024-11-25 14:01:19 +0200

    'build'

--- src/components/table/utils.tsx
+++ src/components/table/utils.tsx
@@ -114,7 +114,7 @@ export const Filter = memo<FilterProps>(({ filterableColumn, index }) => {
                 {filterPopupsDisplay === filterableColumn.dataKey && (
                     <div
                         className={`absolute top-[-20px] ${
-                            displayRight ? "right-[-100%]" : "left-[-100%]"
+                            displayRight ? " left-[100%]" : "right-[100%]"
                         } w-40 h-32 text-black bg-white p-1 flex flex-col items-center gap-2 shadow`}
                         // className={`absolute z-20 top-1 ${displayRight ? "right-[-20%]" : "left-[-20%]"}
                         //           w-40 h-32 text-black bg-white p-1 flex flex-col items-center gap-2 shadow`}
