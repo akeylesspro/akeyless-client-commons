a56cbb2e3a52baa7c2254c3407a40d5c9ab78680
avrhahm sason <avi6190i@gmail.com>
2024-11-25 14:07:51 +0200

    'build'

--- src/components/table/utils.tsx
+++ src/components/table/utils.tsx
@@ -113,7 +113,7 @@ export const Filter = memo<FilterProps>(({ filterableColumn, index }) => {
             <div className="relative">
                 {filterPopupsDisplay === filterableColumn.dataKey && (
                     <div
-                        className={`absolute top-[-20px] ${
+                        className={`absolute top-[-20px] z-20${
                             displayRight ? " left-[100%]" : "right-[100%]"
                         } w-40 h-32 text-black bg-white p-1 flex flex-col items-center gap-2 shadow`}
                         // className={`absolute z-20 top-1 ${displayRight ? "right-[-20%]" : "left-[-20%]"}
