669afcad870b39ab087287d33cb5a61a735da23b
avrhahm sason <avi6190i@gmail.com>
2024-12-22 01:26:14 +0200

    'build'

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -1,3 +1,42 @@
+// import * as React from "react";
+// import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+// import { cn } from "@/lib/utils";
+// type ProgressRef = React.ElementRef<typeof ProgressPrimitive.Root>;
+// interface ProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+//     containerClassName?: string;
+//     indicatorClassName?: string;
+//     showValue?: boolean;
+//     showValueClassName?: string;
+// }
+
+// const ProgressComponent = React.forwardRef<ProgressRef, ProgressProps>(
+//     ({ className, value, containerClassName, indicatorClassName, showValueClassName, showValue = false, ...props }, ref) => (
+//         <div className={cn("relative w-full", containerClassName)}>
+//             <ProgressPrimitive.Root ref={ref} className={cn("relative h-2 w-full overflow-hidden rounded-full bg-primary/20", className)} {...props}>
+//                 <ProgressPrimitive.Indicator
+//                     className={cn("h-full w-full flex-1 bg-primary transition-all", indicatorClassName)}
+//                     style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+//                 >
+//                     {showValue && (
+//                         <div
+//                             className={cn(
+//                                 "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-sm font-medium text-black",
+//                                 showValueClassName
+//                             )}
+//                         >
+//                             {`${value || 0}%`}
+//                         </div>
+//                     )}
+//                 </ProgressPrimitive.Indicator>
+//             </ProgressPrimitive.Root>
+//         </div>
+//     )
+// );
+// ProgressComponent.displayName = ProgressPrimitive.Root.displayName;
+
+// export { ProgressComponent };
+
 import * as React from "react";
 import * as ProgressPrimitive from "@radix-ui/react-progress";
 
@@ -15,20 +54,10 @@ const ProgressComponent = React.forwardRef<ProgressRef, ProgressProps>(
         <div className={cn("relative w-full", containerClassName)}>
             <ProgressPrimitive.Root ref={ref} className={cn("relative h-2 w-full overflow-hidden rounded-full bg-primary/20", className)} {...props}>
                 <ProgressPrimitive.Indicator
-                    className={cn("h-full w-full flex-1 bg-primary transition-all", indicatorClassName)}
-                    style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-                >
-                    {showValue && (
-                        <div
-                            className={cn(
-                                "absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-sm font-medium text-black",
-                                showValueClassName
-                            )}
-                        >
-                            {`${value || 0}%`}
-                        </div>
-                    )}
-                </ProgressPrimitive.Indicator>
+                    className={cn("h-full bg-primary transition-all flex justify-center items-center", indicatorClassName)}
+                    style={{ width: `${value || 0}%` }}
+                />
+                {showValue && <div className={cn("text-sm font-medium text-black", showValueClassName)}>{`${value || 0}%`}</div>}
             </ProgressPrimitive.Root>
         </div>
     )
