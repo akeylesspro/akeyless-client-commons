7396b2e8dbf79e86cf90226aeb639a8e12636a02
avrhahm sason <avi6190i@gmail.com>
2025-05-14 12:52:42 +0300

    /././

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -6,8 +6,11 @@ import { isLocal, mode } from "./global";
 export const baseDomain = mode === "qa" ? "https://nx-api.xyz/api" : "https://nx-api.info/api";
 export const devicesDomain = isLocal ? "http://localhost:9001/api/devices" : baseDomain + "/devices";
 export const biDomain = isLocal ? "http://localhost:9002/api/bi" : baseDomain + "/bi";
-export const callCenterDomain = isLocal ? "http://localhost:9003/api/call-center" : baseDomain + "/call-center";
+export const notificationsDomain = isLocal ? "http://localhost:9006/api/notifications" : baseDomain + "/notifications";
+export const callCenterGeoDomain = isLocal ? "http://localhost:9007/api/call-center/geo" : baseDomain + "/call-center/geo";
+export const callCenterEventsDomain = isLocal ? "http://localhost:9008/api/call-center/events" : baseDomain + "/call-center/events";
 export const akeylessOnlineDomain = mode === "qa" ? "https://akeyless-online.xyz" : "https://akeyless-online.info";
+
 type Method = "GET" | "POST" | "PUT" | "DELETE";
 type ServerName = "devices" | "bi" | "call-center-geo" | "call-center-events" | "notifications";
 
@@ -22,13 +25,13 @@ export const nxApiCall = async (serverName: ServerName, method: Method, url: str
                 urlResult = `${devicesDomain}/${url}`;
                 break;
             case "notifications":
-                urlResult = `${devicesDomain}/${url}`;
+                urlResult = `${notificationsDomain}/${url}`;
                 break;
             case "call-center-events":
-                urlResult = `${callCenterDomain}/call-center/events`;
+                urlResult = `${callCenterEventsDomain}/${url}`;
                 break;
             case "call-center-geo":
-                urlResult = `${callCenterDomain}/call-center/geo`;
+                urlResult = `${callCenterGeoDomain}/${url}`;
                 break;
             default:
                 break;
