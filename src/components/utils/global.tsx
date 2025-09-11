8ff1ab9089864c9cb9bae1f82354fa6dacd7a47e
avrhahm sason <avi6190i@gmail.com>
2025-09-11 12:54:19 +0300

    add title and props to LeftToRightDiv

--- src/components/utils/global.tsx
+++ src/components/utils/global.tsx
@@ -1,14 +1,31 @@
 import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { ComponentProps, ReactNode } from "react";
 import { Direction } from "src/types";
 
 export const Version = ({ version, className = "" }: { version: string; className?: string }) => {
     return <div className={cn("absolute text-black z-30 bottom-[0px] text-xs right-0 px-1 ", className)}>v{version}</div>;
 };
 
-export const LeftToRightDiv = ({ direction, className, children }: { direction: Direction; children: ReactNode; className?: string }) => {
+export const LeftToRightDiv = ({
+    direction,
+    className,
+    children,
+    props,
+    title,
+}: {
+    direction: Direction;
+    children: ReactNode;
+    className?: string;
+    props: ComponentProps<"div">;
+    title?: string;
+}) => {
     return (
-        <div style={{ direction: "ltr" }} className={cn(`_ellipsis  ${direction === "rtl" ? "text-right" : "text-left"}`, className)}>
+        <div
+            title={title}
+            style={{ direction: "ltr" }}
+            className={cn(`_ellipsis  ${direction === "rtl" ? "text-right" : "text-left"}`, className)}
+            {...props}
+        >
             {children}
         </div>
     );
