0ebfc90702144a6e8cc82c8db4ed58c2602576c0
avrhahm sason <avi6190i@gmail.com>
2026-02-24 16:43:48 +0200

    add som functions

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -1,3 +1,11 @@
 export const multiply = (a: number, b: number): number => {
     return a * b;
 };
+
+export const add = (a: number, b: number): number => {
+    return a + b;
+};
+
+export const subtract = (a: number, b: number): number => {
+    return a - b;
+};
